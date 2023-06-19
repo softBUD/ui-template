561e160a00e8c2aa391b1333ecebc3c0c1a8cdd6
kdy9943 <kdy9943@dejay.co.kr>
2023-06-19 16:56:49 +0900

    feat: configure config files

---
+++ prettier.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderMergeDuplicateImports: true,
+  plugins: [
+    require('prettier-plugin-tailwindcss'),
+    '@ianvs/prettier-plugin-sort-imports',
+  ],
+}
